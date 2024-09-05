57d3cedfabcc5d8bbbe277eb24b41dacb207a7e0
hysong4u <hayeon_song@naver.com>
2024-09-05 23:16:19 +0900

    [feat] 가족플라자 정보 & 서울시 사이트 정보 추가

--- huranit-front/src/pages/CenterInfo.jsx
+++ huranit-front/src/pages/CenterInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Head from "../components/Head";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { Button, Divider, Typography } from "@mui/material";
@@ -6,6 +6,9 @@ import styled from "styled-components";
 import Foot from "../components/Foot";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { Link } from "react-router-dom";
+import Card1 from "../assets/img/card01.png";
+import Card2 from "../assets/img/card02.png";
+import Card3 from "../assets/img/card03.png";
 
 const CenterInfo = () => {
   return (
@@ -47,30 +50,67 @@ const CenterInfo = () => {
       <Divider />
       <BodyWrapp style={{ marginTop: "20px" }}>
         <Typography style={{ fontFamily: "NanumSquareNeoBold" }}>
-          서울시여성가족재단에서 운영하는 돌봄서비스를 이용해보세요!
+          서울시여성가족재단에서 운영하는 <br />돌봄서비스를 이용해보세요!
         </Typography>
-        <Typography style={{ fontSize: "14px", marginTop: "5px" }}>
+        <Typography style={{ fontSize: "14px", marginTop: "10px" }}>
           양육자의 돌봄 및 일생활 균형을 지원하고 어린이, 양육자를 포함한 가족 모두를 위한
           공간입니다.
         </Typography>
-        <Typography style={{ color: "#42a5f5", marginTop: "10px", marginBottom: "30px" }}>
+        <Typography style={{ color: "#42a5f5", marginTop: "20px", marginBottom: "30px" }}>
           <Link to="/SeoulPlaza" style={{ textDecoration: "none", color: "#2196f3" }}>
             서울가족플라자 살펴보기
             <ChevronRightIcon style={{ color: "#42a5f5", position: "absolute" }} />
           </Link>
         </Typography>
       </BodyWrapp>
-      <BodyWrapp></BodyWrapp>
+      <BackgroundWrapper>
+        <Typography style={{ fontFamily: "NanumSquareNeoBold", marginTop: "20px"}}>
+          더 나은 일생활 균형 및 <br />양육 친화적 문화를 위해 제공되는 <br />서울시의 유용한 사이트들을 살펴보세요
+        </Typography>
+        <Typography style={{ fontSize: "14px", marginTop: "10px"}}>
+          클릭시 해당 페이지로 이동합니다.
+        </Typography>
+        <ImageWrapper>
+          <img
+            src={Card1}
+            alt="Card 1"
+            style={{ width: "330px", height: "176px", borderRadius: "8px", marginTop: "10px" }}
+          />
+          <img
+            src={Card2}
+            alt="Card 2"
+            style={{ width: "330px", height: "176px", borderRadius: "8px",marginTop: "10px" }}
+          />
+          <img
+            src={Card3}
+            alt="Card 3"
+            style={{ width: "330px", height: "176px", borderRadius: "8px",marginTop: "10px", marginBottom: "20px" }}
+          />
+        </ImageWrapper>
+      </BackgroundWrapper>
       <Foot />
     </div>
   );
 };
 
-const BodyWrapp = styled.body`
+const BodyWrapp = styled.div`
   background-color: white;
   margin-right: 16px;
   margin-left: 16px;
-  margin-botton: 10px;
+  margin-bottom: 10px;
+`;
+
+const BackgroundWrapper = styled.div`
+  background-color: #e3f2fd;  
+  padding: 20px;
+`;
+
+const ImageWrapper = styled.div`
+  display: flex;
+  justify-content: center; 
+  gap: 10px; 
+  flex-wrap: wrap;
+  margin-top: 20px; 
 `;
 
 export default CenterInfo;
